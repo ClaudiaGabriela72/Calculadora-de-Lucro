f35d02935e8e5441016e14d27d32edbcb0607d35
ClaudiaGabriela72 <71900311+ClaudiaGabriela72@users.noreply.github.com>
2024-09-26 15:49:34 -0400

    resultado

--- lucro.py
+++ lucro.py
@@ -1,6 +1,16 @@
+# definindo as variaveis
 preco_compra = float(input('Digite o preço da compra: '))
 preco_venda = float(input('Digite o preço da venda: '))
 custos_adicionais = float(input('Digite os custos adicionais (opicional): '))
-lucro = preco_compra - preco_venda - custos_adicionais
+
+# Calculando o resultado
+lucro = preco_venda - preco_compra - custos_adicionais
+
 
 print('O lucro é de : R$ {:.2f}'.format(lucro))
+
+# calculando a margem de lucro
+margem_lucro = (lucro/preco_venda) * 100
+
+#imprimindo o resultado
+print('A margem de lucro é de {:.2f}%'.format(margem_lucro))
